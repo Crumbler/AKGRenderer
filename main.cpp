3f042c67525db143a82c9b042fb416ee009f7b7f
Crumbler <25861574+Crumbler@users.noreply.github.com>
2022-09-01 13:25:58 +0300

    Initial project

--- main.cpp
+++ main.cpp
@@ -1,9 +1,159 @@
-#include <iostream>
 
-using namespace std;
+#include <cstdio>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+ImGuiIO *io;
+
+void OnMouseMove(GLFWwindow* window, double xpos, double ypos)
+{
+    io->MousePos = ImVec2(xpos, ypos);
+}
+
+void OnMouseButton(GLFWwindow* window, int button, int action, int mods)
+{
+    const bool isPressed = action == GLFW_PRESS;
+
+    switch(button)
+    {
+    case GLFW_MOUSE_BUTTON_LEFT:
+        io->MouseDown[0] = isPressed;
+        break;
+
+    case GLFW_MOUSE_BUTTON_RIGHT:
+        io->MouseDown[1] = isPressed;
+        break;
+
+    case GLFW_MOUSE_BUTTON_MIDDLE:
+        io->MouseDown[2] = isPressed;
+        break;
+    }
+}
+
+void OnScroll(GLFWwindow* window, double xoffset, double yoffset)
+{
+    io->MouseWheel = yoffset;
+}
+
+void OnDisplay(GLFWwindow* window)
+{
+    ImGui::Render();
+
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwSwapBuffers(window);
+}
+
+void OnSize(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void OnFramebufferSize(GLFWwindow* window, int width, int height)
+{
+
+}
+
+void GUI_Main(GLFWwindow *window)
+{
+    ImGui::Begin("MainWindow", nullptr,
+                 ImGuiWindowFlags_NoMove |
+                 ImGuiWindowFlags_NoResize |
+                 ImGuiWindowFlags_NoCollapse |
+                 ImGuiWindowFlags_NoTitleBar |
+                 ImGuiWindowFlags_MenuBar);
+
+    ImGui::End();
+}
+
+void OnInit()
+{
+    io->IniFilename = nullptr;
+}
+
+void OnUpdate(GLFWwindow *window)
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+
+    ImGui::NewFrame();
+
+    GUI_Main(window);
+}
+
+void OnExit()
+{
+
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(1280, 720,
+                              "DLAGL", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        fprintf(stderr, "Failed to initialize GLAD\n");
+        return -1;
+    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    io = &ImGui::GetIO();
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+
+    glfwSetCursorPosCallback(window, OnMouseMove);
+    glfwSetMouseButtonCallback(window, OnMouseButton);
+    glfwSetScrollCallback(window, OnScroll);
+    glfwSetWindowSizeCallback(window, OnSize);
+    glfwSetFramebufferSizeCallback(window, OnFramebufferSize);
+
+    OnInit();
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        OnUpdate(window);
+
+        OnDisplay(window);
+    }
+
+    OnExit();
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
